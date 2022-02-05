ed2dd77ce6230443d5a311c01564005b9f926886
Ben Gross <benrgross@gmail.com>
2022-02-05 12:42:19 -0500

    nav style

--- components/Nav.js
+++ components/Nav.js
@@ -1,42 +1,54 @@
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0";
 
 function Nav() {
+  const { user } = useUser();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid navbar-brand nav-icon">
-        <Link className="navbar-brand hello" href="/">
+      <a
+        style={{ color: "blue" }}
+        className="navbar-brand nav-start
+    "
+      >
+        <Link className="navbar-brand" href="/">
           Swell Clutch
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        {user ? (
+          <>
             <li className="nav-item nav-link active nav__item-link nav__item-link-first">
               <Link className="nav-link active" aria-current="page" href="/">
                 Dashboard
               </Link>
+              <li className="nav-item nav-link active nav__item-link">
+                <Link className="nav-link" href="/api/auth/logout">
+                  logout
+                </Link>
+              </li>
             </li>
-            <li className="nav-item nav-link active nav__item-link">
-              <Link className="nav-link" href="/api/auth/login">
-                Sign In
-              </Link>
-            </li>
-            <li className="nav-item nav-link active nav__item-link">
-              <Link className="nav-link" href="/api/auth/logout">
-                logout
+          </>
+        ) : (
+          <ul className="navbar-nav ml-auto nav-sign-in-out">
+            <li className="nav-item  ">
+              <Link href="/api/auth/login">
+                <a className="nav-link active">Sign In</a>
               </Link>
             </li>
           </ul>
-        </div>
+        )}
       </div>
     </nav>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { useUser } from "@auth0/nextjs-auth0";
 import styles from "../styles/Home.module.css";
 

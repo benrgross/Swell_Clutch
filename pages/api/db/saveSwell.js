dbe8624f24618929d3dc2150e231db36e9ed912e
Ben Gross <benrgross@gmail.com>
2022-02-08 17:10:25 -0500

    prisma post for save swell not working

--- pages/api/db/saveSwell.js
+++ pages/api/db/saveSwell.js
@@ -5,36 +5,7 @@ export default async function handler(req, res) {
   const { spot } = req.body;
   if (req.method === "POST") {
     try {
-      const session = await prisma.session.create({
-        data: {
-          timeStamp: spot.tide.current.timestamp,
-          utcOffset: spot.tide.current.utcOffset,
-          timeZone: spot.timezone,
-          surfMin: spot.waveHeight.min,
-          surfMax: spot.waveHeight.max,
-          waterTemp: `${spot.waterTemp.min} -  ${spot.waterTemp.max}`,
-          location: {
-            connectOrCreate: {
-              where: {
-                id: spot._id,
-              },
-              create: {
-                id: spot._id,
-                name: spot.name,
-                lat: spot.lat,
-                spot: spot.lan,
-                timezone: spot.timezone,
-                thumbnail: spot.thumbnail,
-                type: spot.location.type,
-                subreagionId: spot.subreagionId,
-              },
-            },
-          },
-          include: {
-            location: true,
-          },
-        },
-      });
+      const session = await prisma.session.create({});
       console.log(session);
       res.status(200).send(session);
     } catch (err) {

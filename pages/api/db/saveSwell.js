f63cf2682e81384c3d6cb41e2c359cc33805527a
Ben Gross <benrgross@gmail.com>
2022-02-09 14:56:02 -0500

    prisma schema changes with no migration

--- pages/api/db/saveSwell.js
+++ pages/api/db/saveSwell.js
@@ -3,11 +3,120 @@ import prisma from "../../../lib/prisma";
 
 export default async function handler(req, res) {
   const { spot } = req.body;
+  const { _id } = spot;
+  const { swells } = req.body;
+  console.log(req.body.user);
+  console.log(swells);
+  const spotId = parseFloat(_id);
   if (req.method === "POST") {
     try {
-      const session = await prisma.session.create({});
-      console.log(session);
-      res.status(200).send(session);
+      const user = await prisma.user.findUnique({
+        where: {
+          email: req.body.user.email,
+        },
+      });
+      console.log(user);
+      const location = await prisma.location.upsert({
+        where: {
+          name: spot.name,
+        },
+        create: {
+          spot_id: spotId,
+          name: spot.name,
+          lat: spot.lat,
+          lon: spot.lon,
+          timezone: spot.timezone,
+          thumbnail: spot.thumbnail,
+          type: spot.location.type,
+          subregionId: spot.subregionId,
+          user: {
+            connect: {
+              email: req.body.user.email,
+            },
+          },
+          session: {
+            create: {
+              timeStamp: spot.tide.current.timestamp,
+              timeZone: spot.timezone,
+              utcOffset: spot.tide.current.utcOffset,
+              surfMax: spot.waveHeight.max,
+              surfMin: spot.waveHeight.min,
+              waterTemp: `${spot.waterTemp.min} - ${spot.waterTemp.max}`,
+              conditions: {
+                human: spot.conditions.human,
+                value: spot.conditions.value,
+                sortableCondition: spot.conditions.sortableCondition,
+              },
+              tide: {
+                type: spot.tide.current.type,
+                height: spot.tide.current.height,
+                timestamp: spot.tide.timestamp,
+                nextType: spot.tide.next.type,
+                nextTime: spot.tide.next.timestamp,
+                nextHeight: spot.tide.next.height,
+              },
+              wind: {
+                speed: spot.wind.speed,
+                direction: spot.wind.direction,
+                directionType: spot.wind.directionType,
+              },
+              locationId: spotId,
+              userEmail: req.body.user.email,
+            },
+          },
+        },
+        update: {
+          spot_id: spotId,
+          name: spot.name,
+          lat: spot.lat,
+          lon: spot.lon,
+          timezone: spot.timezone,
+          thumbnail: spot.thumbnail,
+          type: spot.location.type,
+          subregionId: spot.subregionId,
+          user: {
+            connect: {
+              email: req.body.user.email,
+            },
+          },
+          session: {
+            create: {
+              timeStamp: spot.tide.current.timestamp,
+              timeZone: spot.timezone,
+              utcOffset: spot.tide.current.utcOffset,
+              surfMax: spot.waveHeight.max,
+              surfMin: spot.waveHeight.min,
+              waterTemp: `${spot.waterTemp.min} - ${spot.waterTemp.max}`,
+              conditions: {
+                human: spot.conditions.human,
+                value: spot.conditions.value,
+                sortableCondition: spot.conditions.sortableCondition,
+              },
+              tide: {
+                type: spot.tide.current.type,
+                height: spot.tide.current.height,
+                timestamp: spot.tide.timestamp,
+                nextType: spot.tide.next.type,
+                nextTime: spot.tide.next.timestamp,
+                nextHeight: spot.tide.next.height,
+              },
+              wind: {
+                speed: spot.wind.speed,
+                direction: spot.wind.direction,
+                directionType: spot.wind.directionType,
+              },
+              locationId: spotId,
+              userEmail: req.body.user.email,
+            },
+          },
+        },
+        include: {
+          session: true,
+          user: true,
+        },
+      });
+
+      res.status(200).send(location);
     } catch (err) {
       console.log(err);
     }

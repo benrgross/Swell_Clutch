963c97d0c281f01aae20a87a808b855f1ead9fcd
Ben Gross <benrgross@gmail.com>
2022-02-09 15:06:06 -0500

    commit before pull

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,11 @@ import Header from "../components/Header";
 import { useUser } from "@auth0/nextjs-auth0";
 import GetStarted from "../components/GetStarted";
 import Dashboard from "../components/Dashboard";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const { user } = useUser();
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Swell</title>
         <meta name="description" content="Surf App for Saving Swells" />

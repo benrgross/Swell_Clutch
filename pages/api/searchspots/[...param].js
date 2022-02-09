4ed41b31e5ab75e13a9baa9825b97800e37f6831
Ben Gross <benrgross@gmail.com>
2022-02-09 17:49:17 -0500

    json fix

--- pages/api/searchspots/[...param].js
+++ pages/api/searchspots/[...param].js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
       console.log(req.body);
+
       const results = [];
       const { data } = await axios.get(
         `https://www.surfline.com/search/${req.query.param}`
@@ -26,7 +27,7 @@ export default async function handler(req, res) {
         results.push(spot);
       });
       console.log(results);
-      res.status(200).send(results);
+      res.status(200).json(results);
     } catch (error) {
       res.status(400).send({ error: "this is a cheerio error" });
       console.log(error);

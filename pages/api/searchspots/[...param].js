8146e52a5fd9536c6195b5bb18d079d887dd5b31
Ben Gross <benrgross@gmail.com>
2022-02-09 18:25:10 -0500

    debug

--- pages/api/searchspots/[...param].js
+++ pages/api/searchspots/[...param].js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
       console.log(req.body);
-
+      res.json(req.body);
       const results = [];
       const { data } = await axios.get(
         `https://www.surfline.com/search/${req.query.param}`
@@ -27,10 +27,11 @@ export default async function handler(req, res) {
         results.push(spot);
       });
       console.log(results);
-      res.status(200).json(results);
+      // res.status(200).json(results);
     } catch (error) {
-      res.status(400).send({ error: "this is a cheerio error" });
-      console.log(error);
+      res.json(error);
+      // res.status(400).send({ error: "this is a cheerio error" });
+      // console.log(error);
     }
   } else {
     res.status(400).send({ error: "server did not receive corrent method" });

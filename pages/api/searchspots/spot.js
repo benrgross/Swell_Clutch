9ca120ecb1f369cc7146e2815d2828858178880f
Ben Gross <benrgross@gmail.com>
2022-02-13 15:40:31 -0500

    vercel test

--- pages/api/searchspots/spot.js
+++ pages/api/searchspots/spot.js
@@ -17,6 +17,10 @@ export default async function handler(req, res) {
       // const context = await browser.newContext();
       const page = await browser.newPage();
 
+      await page.setUserAgent(
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+      );
+
       const url = `https://www.surfline.com/search/${req.body.spot}`;
 
       await page.goto(url);
@@ -74,9 +78,6 @@ export default async function handler(req, res) {
 // );
 
 // const page = await browser.newPage();
-// await page.setUserAgent(
-//   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-// );
 
 // const text = await page.evaluate(() => {
 //   // const name = Array.from(

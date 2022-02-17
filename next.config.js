2eabcba2f79c47d1432f717954c120294d00a62e
Ben Gross <benrgross@gmail.com>
2022-02-17 09:24:52 -0500

    casche off

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   headers: {
+    "X-Vercel-Cache": "BYPASS",
     "User-Agent":
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
   },

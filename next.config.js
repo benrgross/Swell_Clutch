45c87f39eb37c9196f86a92f45217204f79323c0
Ben Gross <benrgross@gmail.com>
2022-02-17 09:22:03 -0500

    header change in next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+  },
 };
 
 // module.exports = {
